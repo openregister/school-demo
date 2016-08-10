de2af249b21508effb903cdc97adbf340fb2351d
robmckinnon <rob@movingflow>
2016-08-10 11:28:25 +0100

    Show schools on map on schools list view
    Use leaflet and openstreetmap to render map in browser and add school markers.

--- app/controllers/points_controller.rb
+++ app/controllers/points_controller.rb
@@ -5,7 +5,8 @@ class PointsController < ApplicationController
     if school = Item.school_at(point)
       redirect_to school_url(id: school.record)
     else
-      @schools = Item.nearest_schools(point)
+      @lon, @lat = Item.lon_lat point
+      @schools = Item.not_ended.nearest_schools(point)
     end
   end
 
