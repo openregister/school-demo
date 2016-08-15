486691a2947b24bdd8d966d6f2bde11ce72771af
robmckinnon <rob@movingflow>
2016-08-15 16:32:43 +0100

    When single match for term, redirect to view for match
    When item search term matches single school, redirect to school view.
    
    When item search term matches single place, redirect to place view.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,7 +6,18 @@ class ItemsController < ApplicationController
   end
 
   def index
-    @items = Item.search params[:q].to_s.strip.chomp(',') if params[:q]
+    if params[:q]
+      @items = Item.search params[:q].to_s.strip.chomp(',')
+      if @items.try(:size) == 1
+        item = @items.first
+        case item.register
+        when 'place'
+          redirect_to point_url(id: item.coordinates.join(','))
+        when 'school'
+          redirect_to school_url(id: item.record)
+        end
+      end
+    end
   end
 
 end
